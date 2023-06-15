3bdc9b3d1dd870fb047efb0ce464d9169f3b23f4
Omokpo Theresa <omokpotessy@gmail.com>
2023-06-15 20:40:36 +0100

    added a new site

--- src/components/MediaCard.jsx
+++ src/components/MediaCard.jsx
@@ -1,17 +1,27 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Staycurious from '../images/Staycurious.jpeg'
+import shopify from '../images/shopify.jpg'
+import estore from '../images/estore.jpg'
 import './MediaCard.css'
+import { Container } from 'react-bootstrap'
 
 export default function MediaCard() {
   return (
-    <div>
+    <div className='pictures'> 
         <div className="card">
-  <img className="card-img-top" src={Staycurious} alt="blogimage" />
+  <img className="card-img-top" src={shopify} alt="blogimage" />
   <div className="card-body">
     <h3 class="card-title">Blog App</h3>
     <p class="card-text">A fully responsive blog website</p>
     <Link className='site' to='https://gisti.netlify.app'>Go to site</Link>
+  </div>
+</div>
+<div className="card">
+  <img className="card-img-top" src={estore} alt="blogimage" />
+  <div className="card-body">
+    <h3 class="card-title">Blog App</h3>
+    <p class="card-text">A fully responsive blog website</p>
+    <Link className='site' to='https://etess.netlify.app'>Go to site</Link>
   </div>
 </div>
 
